02a8e9b2a1663620b14cfd987ab8a5d67eb5f7c1
BK <brian_j_king@me.com>
2020-08-27 14:46:13 -0500

    Done

--- OverloadingMethods/Program.cs
+++ OverloadingMethods/Program.cs
@@ -17,19 +17,38 @@ namespace OverloadingMethods
 
        static string Add(decimal depostitOne, decimal depositTwo, bool didYouBuyAnything)
        {
-            if (didYouBuyAnything == true)
-            {
-                Console.WriteLine($"{depostitOne + depositTwo} dollars");
-            }
+            var message = "I didn't buy anything";
             
-            return "I didn't buy anything";   
+
+            if (didYouBuyAnything)
+            {
+                Console.WriteLine(didYouBuyAnything);
+                
+                var total = Add(depostitOne, depositTwo);
+
+                if (total != 1 && didYouBuyAnything == true )
+                {
+                    message = $"{total} dollars";
+                }
+                else 
+                { 
+                    message = $"{total} dollar";
+                }
+            }
+
+            return message;
+             
        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine($"Sum of Ints {Add(3,4)}");
             Console.WriteLine($"Dec {Add(3m, 4m)}");
-            Add(4m, 5m, true);
+            Console.WriteLine(Add(4m, 5m, true));
+            Console.WriteLine(Add(4m, 5m, false));
+            //Add(4m, 5m, false);
+            //Add(0m, 0m, true);
+            //Add(0m, 0m, false);
         }
     }
 }
